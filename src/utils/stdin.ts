5d7ad5d359c801bf30a6880a0600fb937b6f47af
Polygons1 <47899327+Polygons1@users.noreply.github.com>
2023-11-15 09:20:49 +0200

    Create stdin.ts
    Added stdin file (for input function (ALPHA))

---
+++ src/utils/stdin.ts
@@ -0,0 +1,31 @@
+import * as readline from 'readline/promises';
+
+class Manager {
+    rl?: readline.Interface
+}
+
+Manager.prototype.rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+async function getSTDIN(prompt: string) {
+    return await Manager.prototype.rl.question(prompt)
+}
+
+getSTDIN.closed = false
+
+getSTDIN.close = function close() {
+    Manager.prototype.rl.close()
+    getSTDIN.closed = true
+    Manager.prototype.rl = null
+}
+
+getSTDIN.open = function open() {
+    Manager.prototype.rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+}
+
+export default getSTDIN
